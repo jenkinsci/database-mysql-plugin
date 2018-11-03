f37e257bdfdb9790f20bf825a5931bbdf354e0d1
Cyrille Le Clerc <cleclerc@cloudbees.com>
2018-11-04 08:05:02 +0900

    JENKINS-54434 Bump MySql driver to 8.0.13
    Remove JDBC URL properties validation as the list of supported properties is no longer available because com.mysql.jdbc.ConnectionPropertiesImpl has been removed in v8.x

--- src/main/java/org/jenkinsci/plugins/database/mysql/MySQLDatabase.java
+++ src/main/java/org/jenkinsci/plugins/database/mysql/MySQLDatabase.java
@@ -1,22 +1,10 @@
 package org.jenkinsci.plugins.database.mysql;
 
-import com.mysql.jdbc.Driver;
-import groovy.lang.GroovyShell;
 import hudson.Extension;
-import hudson.Util;
-import hudson.util.FormValidation;
 import hudson.util.Secret;
 import org.jenkinsci.plugins.database.AbstractRemoteDatabase;
 import org.jenkinsci.plugins.database.AbstractRemoteDatabaseDescriptor;
 import org.kohsuke.stapler.DataBoundConstructor;
-import org.kohsuke.stapler.QueryParameter;
-
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-import java.util.Map;
-import java.util.Properties;
-import java.util.Set;
 
 /**
  * @author Kohsuke Kawaguchi
@@ -28,8 +16,8 @@ public class MySQLDatabase extends AbstractRemoteDatabase {
     }
 
     @Override
-    protected Class<Driver> getDriverClass() {
-        return Driver.class;
+    protected Class<com.mysql.cj.jdbc.Driver> getDriverClass() {
+        return com.mysql.cj.jdbc.Driver.class;
     }
 
     @Override
@@ -40,31 +28,10 @@ public class MySQLDatabase extends AbstractRemoteDatabase {
     @Extension
     public static class DescriptorImpl extends AbstractRemoteDatabaseDescriptor {
 
-        private volatile Set<String> validPropertyNames;
-
         @Override
         public String getDisplayName() {
             return "MySQL";
         }
 
-        public FormValidation doCheckProperties(@QueryParameter String properties) throws IOException {
-            try {
-                if (validPropertyNames==null) {
-                    // this computation depends on the implementation details of MySQL JDBC connector
-                    GroovyShell gs = new GroovyShell(getClass().getClassLoader());
-                    validPropertyNames = (Set<String>) gs.evaluate ( new InputStreamReader ( MySQLDatabase.class.getResourceAsStream ( "validate.groovy" ),
-                        Charset.forName ( "UTF-8" ) ) );
-                }
-                Properties props = Util.loadProperties(properties);
-                for (Map.Entry e : props.entrySet()) {
-                    String key = e.getKey().toString();
-                    if (!validPropertyNames.contains(key))
-                        return FormValidation.error("Unrecognized property: "+key);
-                }
-                return FormValidation.ok();
-            } catch (Throwable e) {
-                return FormValidation.warning(e,"Failed to validate the connection properties");
-            }
-        }
     }
 }
